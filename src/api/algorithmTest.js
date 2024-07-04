3d9ec06116d2044e678cf2824e15428288347152
qazplm12 <yeah_chef@naver.com>
2024-07-04 12:37:47 +0900

    배포 환경에 맞추어 수정

--- src/api/algorithmTest.js
+++ src/api/algorithmTest.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+const host = import.meta.env.VITE_API_HOST;
+
 
 export const registerAlgorithm = async (formData) => {
   try {
-    const response = await axios.post("http://118.129.145.98:1880/allgo_register", formData);
+    const response = await axios.post(`${host}/allgo_register`, formData);
     console.log(response.data);
     return response.data;
   } catch (err) {
--- src/pages/MainPage/MainSection2.jsx
+++ src/pages/MainPage/MainSection2.jsx
@@ -6,7 +6,7 @@ function renderInput(input, key, formData, handleChange) {
   if (!input) return null;
 
   switch (input.detail.type) {
-    case 'input':
+    case 'string' :
       return (
         <div key={key} className="flex mb-2.5">
           <label className="w-1/2 block my-auto">{input.name}</label>
@@ -77,6 +77,7 @@ function groupInputs(inputs) {
 
 // 입력 폼 생성기
 function FormGenerator({algorithmData, formData, handleChange}) {
+  console.log(algorithmData)
   if (!algorithmData || !algorithmData.algorithm_input) return null;
 
   const groupedInputs = groupInputs(algorithmData.algorithm_input);
@@ -126,7 +127,7 @@ function MainSection2({selectedAlgorithm}) {
       }
     }
 
-    transformedData.name = selectedAlgorithm[0].algorithm_name;
+    transformedData.name = selectedAlgorithm.algorithm_name;
     transformedData.fuels = fuels;
     return transformedData;
   };
@@ -157,13 +158,13 @@ function MainSection2({selectedAlgorithm}) {
   if (selectedAlgorithm !== null)
     return (
       <div className="bg-gray-100 p-4 rounded h-full">
-        <h2 className="text-2xl font-bold mb-8 pb-0">{selectedAlgorithm[0].algorithm_name}</h2>
+        <h2 className="text-2xl font-bold mb-8 pb-0">{selectedAlgorithm.algorithm_name}</h2>
         <div className="grid grid-cols-12 p-3 bg-white rounded-xl">
 
           {/* 입력 섹션 */}
           <div className="col-span-6 m-4 p-4 border-2 border-gray-300 rounded-lg">
             <h3 className="font-bold mb-2">입력</h3>
-            <FormGenerator algorithmData={selectedAlgorithm[0]} formData={formData} handleChange={handleChange}/>
+            <FormGenerator algorithmData={selectedAlgorithm} formData={formData} handleChange={handleChange}/>
             <div className="flex justify-center">
               <button onClick={handleSubmit}
                       className="w-3/5 mt-6 p-2 bg-Cmain text-white font-bold text-lg rounded-xl">
@@ -175,12 +176,12 @@ function MainSection2({selectedAlgorithm}) {
           {/* 출력 섹션 */}
           <div className="col-span-6 m-4 p-4 border-2 border-gray-300 rounded-lg">
             <h3 className="font-bold mb-2">출력</h3>
-            {selectedAlgorithm[0].algorithm_output && Object.entries(output).map(([k, v], idx) => (
+            {selectedAlgorithm.algorithm_output.map((item, idx) => (
               <div key={idx} className="flex mb-2.5">
-                <label className="w-1/2 block my-2">{k || ''}</label>
+                <label className="w-1/2 block my-2">{item.name || ''}</label>
                 <input
                   type="text"
-                  value={v || ''}
+                  value={Object.values(output)[idx] || ''}
                   className="w-1/2 p-2 border rounded text-right"
                   readOnly
                 />
